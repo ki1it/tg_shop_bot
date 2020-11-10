dd7f503d643f4fc82e3b5181bd4c3051ba43030a
Kirill Shulga <kirsh.ru@yandex.ru>
2020-11-10 18:58:47 +0300

    add echo bot

--- go.mod
+++ go.mod
@@ -4,4 +4,5 @@ go 1.15
 
 require (
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
-import "os"
+import (
+	"encoding/json"
+	"fmt"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
 
 func getEnv(key string, def string) string {
 	val, ok := os.LookupEnv(key)
@@ -10,31 +18,47 @@ func getEnv(key string, def string) string {
 	return val
 }
 
-
-func main() {
-	
+func gracefulExit(w http.ResponseWriter, text string) {
+	log.Println(text)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
 
-package main
-
-import (
-    "fmt"
-    "net/http"
-)
-
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello World")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		token, ok := os.LookupEnv("BOT_TOKEN")
+		if !ok {
+			gracefulExit(w, "no telegram bot token")
+			return
+		}
+		bot, err := tgbotapi.NewBotAPI(token)
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			gracefulExit(w, "no body")
+			return
+		}
+		var update tgbotapi.Update
+		if err := json.Unmarshal(body, &update); err != nil {
+			gracefulExit(w, "body parse error")
+			return
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		// todo: looks like send message can be done via response on this webhook request
+		// but let's use this API for now
+		bot.Send(msg)
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
 	})
+
+	http.HandleFunc("/greet/", func(w http.ResponseWriter, r *http.Request) {
+		name := r.URL.Path[len("/greet/"):]
+		fmt.Fprintf(w, "Hello %s\n", name)
+	})
+
+	http.ListenAndServe(":9990", nil)
 	
-	http.HandleFunc("/basepoint/update/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello")
-    })
-
-    http.HandleFunc("/greet/", func(w http.ResponseWriter, r *http.Request) {
-        name := r.URL.Path[len("/greet/"):]
-        fmt.Fprintf(w, "Hello %s\n", name)
-    })
-
-    http.ListenAndServe(":9990", nil)
 }
